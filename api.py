5bf49d46956165b5bcfa60321f388a35901df96a
Kirill456Z <Kirill456Z@yandex.ru>
2023-02-26 16:16:46 +0300

    artist and song search added

---
+++ api.py
@@ -0,0 +1,46 @@
+from lyricsgenius import Genius
+
+token = "m0V_r7JSXoNErub9tLrPSNa7a3MfQYsF2oHsqkOEmysZZm7Bk-fwdLrYNrAVgfqc"
+genius = Genius(token)
+
+
+def get_songs_formated(search_text: str):
+    results = genius.search_songs(search_text)
+    num_results = len(results["hits"])
+    if num_results == 0:
+        return {"status": 0}
+    else:
+        response = "Here's top 10 results:\n"
+        formated_songs = []
+        ids = []
+        for i in range(min(num_results, 10)):
+            cur_res = results["hits"][i]["result"]
+            title = cur_res["title"]
+            artist = cur_res["primary_artist"]["name"]
+            formated_songs.append(f"{i + 1}. {title} by {artist}")
+            ids.append(cur_res["id"])
+        response += "\n".join(formated_songs)
+    return {"status": 1, "formated": response, "ids": ids}
+
+def get_song_info(song_id : int):
+    song = genius.song(song_id)['song']
+    title = song['title']
+    author = song['primary_artist']['name']
+    album = song['album']['name']
+    views = song['stats']['pageviews']
+    url = song['url']
+    return f'artist: {author}\ntitle: {title}\nalbum: {album}\nviews: {views}\nurl: {url}'
+
+
+def search_artists(search_text : str):
+    res = genius.search_artists(search_text, per_page = 10)['sections'][0]['hits']
+    if len(res) == 0:
+        return {'status' : 0}
+    formated_artists = []
+    ids = []
+    for i in range(len(res)):
+        cur_artist = res[i]['result']
+        ids.append(cur_artist['id'])
+        formated_artists.append(f'{i+1}. {cur_artist["name"]}')
+    res_str = "\n".join(formated_artists)
+    return {'status' : 1, 'formated' : res_str, 'ids' : ids}
--- index.py
+++ index.py
@@ -1,46 +1,125 @@
-from dff.script import Actor, TRANSITIONS, RESPONSE, Message
+from dff.script import Actor, TRANSITIONS, RESPONSE, Message, Context, NodeLabel3Type
+import dff.script.labels as lbl
 from dff.pipeline import Pipeline
 import dff.script.conditions as cnd
 from dff.messengers.telegram import PollingTelegramInterface
 from conf import BOT_TOKEN
 
-from dff.utils.testing.common import (
-    check_happy_path,
-    is_interactive_mode,
-    run_interactive_mode,
-)
+from api import get_songs_formated, get_song_info, search_artists
 
+### Transitions
+
+def request_song_transition(ctx: Context, actor: Actor, *args, **kwargs) -> NodeLabel3Type:
+    message = ctx.last_request.text
+    if message == 'cancel':
+        return ('greeting_flow', 'request_task', 1.0)
+    search_result = get_songs_formated(message)
+    if search_result['status'] == 0:
+        return ('song_search_flow', 'no_songs_found', 1.0)
+    else:
+        ctx.misc['song_search_res'] = search_result
+        return ('song_search_flow', 'print_results', 1.0)
+
+def request_artist_transition(ctx: Context, actor: Actor, *args, **kwargs) -> NodeLabel3Type:
+    message = ctx.last_request.text
+    if message == 'cancel':
+        return ('greeting_flow', 'request_task', 1.0)
+    search_result = search_artists(message)
+    if search_result['status'] == 0:
+        return ('artist_search_flow', 'no_songs_found', 1.0)
+    else:
+        ctx.misc['artist_search_res'] = search_result
+        return ('artist_search_flow', 'print_results', 1.0)
+
+### Responses
+
+def print_song_search_results(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
+    response = 'Here\'s what i\'ve found:\n'
+    response += ctx.misc['song_search_res']['formated'] + '\n\n'
+    response += 'print song number if you\'d like to get more information about it'
+    return Message(text = response)
+
+def print_artist_search_results(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
+    response = 'Here\'s what i\'ve found:\n'
+    response += ctx.misc['artist_search_res']['formated']
+    return Message(text = response)
+
+def print_song_info(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
+    msg = ctx.last_request.text
+    try: 
+        song_num = int(msg)
+    except Exception as e:
+        return Message(text = 'ooops')
+    song_num = int(msg) - 1
+    id = ctx.misc['song_search_res']['ids'][song_num]
+    return Message(text = get_song_info(id))
+
+### Conditions
+
+def is_number_given(min = 1, max = 10):
+    def check_num(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
+        msg = ctx.last_request.text
+        try:
+            index = int(msg)
+        except Exception as e:
+            return False
+        return min <= index and index <= max
+
+    return check_num
+
+    
 toy_script = {
     "greeting_flow": {
-        "start_node": {  # This is the initial node,
-            # it doesn't contain a `RESPONSE`.
+        'start_node': {
             RESPONSE: Message(),
-            TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
-            # If "Hi" == request of the user then we make the transition.
+            TRANSITIONS: {'request_task' : cnd.exact_match(Message(text="/start"))}
         },
-        "node1": {
-            RESPONSE: Message(text="Hi, how are you?"),  # When the agent enters node1,
-            # return "Hi, how are you?".
-            TRANSITIONS: {"node2": cnd.exact_match(Message(text="I'm fine, how are you?"))},
-        },
-        "node2": {
-            RESPONSE: Message(text="Good. What do you want to talk about?"),
-            TRANSITIONS: {"node3": cnd.exact_match(Message(text="Let's talk about music."))},
-        },
-        "node3": {
-            RESPONSE: Message(text="Sorry, I can not talk about music now."),
-            TRANSITIONS: {"node4": cnd.exact_match(Message(text="Ok, goodbye."))},
-        },
-        "node4": {
-            RESPONSE: Message(text="Bye"),
-            TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
+        "request_task": {
+            RESPONSE: Message(text="Hi, i can get information about songs from Genius.com.\
+                               What would you like to find a song or an artist?"),
+            TRANSITIONS: {('song_search_flow', 'start_node', 2.0) : cnd.exact_match(Message(text="Song")),
+                          ('artist_search_flow', 'start_node', 2.0) : cnd.exact_match(Message(text="Artist")),
+                          'fallback_node' : cnd.true()},
         },
         "fallback_node": {
-            # We get to this node if the conditions
-            # for switching to other nodes are not performed.
-            RESPONSE: Message(text="Ooops"),
-            TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
+            RESPONSE: Message(text="Sorry, i dont understand you. Specify type of content you would like to find (Artist/Song)"),
+            TRANSITIONS: {('song_search_flow', 'start_node', 2.0) : cnd.exact_match(Message(text="Song")),
+                          ('artist_search_flow', 'start_node', 2.0) : cnd.exact_match(Message(text="Artist")),
+                          'fallback_node' : cnd.true()},
+        }
+    },
+    "song_search_flow" : {
+        "start_node": {
+            RESPONSE: Message(text = 'Input lyrics you would like to find'),
+            TRANSITIONS: {request_song_transition : cnd.true()}
         },
+        'no_songs_found': {
+            RESPONSE: Message(text = 'I couldn\'t find any songs with such lyrics, try something else'),
+            TRANSITIONS: {request_song_transition: cnd.true()}
+        },
+        'print_results': {
+            RESPONSE: print_song_search_results, 
+            TRANSITIONS: {('song_search_flow', 'print_info', 2) : is_number_given(1,10),
+                          ('greeting_flow', 'request_task', 1.5) : cnd.true()}
+        },
+        'print_info' : {
+            RESPONSE: print_song_info,
+            TRANSITIONS: {('greeting_flow', 'request_task', 1) : cnd.true()}
+        }
+    },
+    "artist_search_flow": {
+        "start_node": {
+            RESPONSE: Message(text ="What artist would you like to find?"),
+            TRANSITIONS: {request_artist_transition : cnd.true()}
+        },
+        'print_results': {
+            RESPONSE : print_artist_search_results,
+            TRANSITIONS: {('greeting_flow', 'request_task', 1) : cnd.true()}
+        },
+        'no_artist_found': {
+            RESPONSE: Message(text = 'I couldn\t find any artists, try something else'),
+            TRANSITIONS: {request_artist_transition : cnd.true()}
+        }
     }
 }
 
